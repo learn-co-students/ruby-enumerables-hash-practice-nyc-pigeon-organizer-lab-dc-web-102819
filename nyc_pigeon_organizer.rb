555deddfeefb1b0b937dfa3962535b5bafc49068
eunice kim <oonis86@gmail.com>
2019-10-25 13:58:54 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,4 @@
-pigeons = {
+pigeon_data = {
   :color => {
     :purple => ["Theo", "Peter Jr.", "Lucky"],
     :grey => ["Theo", "Peter Jr.", "Ms. K"],
@@ -17,33 +17,49 @@ pigeons = {
   }
 }
 
-def nyc_pigeon_organizer(data)
+def nyc_pigeon_organizer (data)
+  final = {}
 
-  names = []
-  pigeon_hash = {}
-
-  data.each do |attribute, items|
-    items.each do |feature, arr|
-      arr.each do |name|
-        names << name if !names.include?(name)
-      end
-    end
-  end
+  data.each do |first_level, all_other|
+    all_other.each do |category, array|
+      array.each do |name|
+        final[name] = {:color => [], :gender => [], :lives => []}
+      end 
+    end 
+  end 
+  x = final.keys
+  data[:color].each do |bird_color, name|
+    name.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          final[item][:color] << bird_color.to_s
+        end 
+      end 
+    end 
+  end 
+  data[:gender].each do |gender, type|
+    type.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          final[item][:gender] << gender.to_s
+        end 
+      end 
+    end 
+  end 
+  data[:lives].each do |location, name|
+    name.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          final[item][:lives] << location
+        end 
+      end 
+    end 
+  end 
   
-  names.each do |nombre|
-    pigeon_hash[nombre] = Hash.new {|k, v| k[v] = []}
-    data.each do |attribute, items|
-      pigeon_hash[nombre][attribute]
-      items.each do |feature, arr|
-        arr.each do |name|
-          pigeon_hash[name][attribute] << feature.to_s if name == nombre
-        end
-      end
-    end
-  end
-  
-  pigeon_hash
+  return final 
+end 
 
-end
+nyc_pigeon_organizer(pigeon_data)
 
-nyc_pigeon_organizer(pigeons)
+
+
